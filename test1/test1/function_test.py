ab81beb83a888ec417bf406561fd4f4ce72a413c
ABCBOLIWAWA <976775221@qq.com>
2016-11-19 13:03:14 +0800

    v4

--- test1/test1/function_test.py
+++ test1/test1/function_test.py
@@ -1,6 +1,19 @@
 
 from selenium import webdriver
-browser =webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert'Django' in browser.title
-print 3
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser =webdriver.Firefox()
+        self.browser.implicitly_wait(20)
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To_Do",self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__== '__main__':
+    unittest.main()
+        
